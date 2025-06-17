ab42a3349405a7ec7ba0f24de3ebbd5a3f5c663a
Ykuyo <ykuyo.dev@gmail.com>
2025-06-16 22:36:05 -0700

    Moved to test-only buckets.

--- local/env.ts
+++ local/env.ts
@@ -3,6 +3,6 @@ import "@phading/cluster/dev/env";
 import { ENV_VARS } from "../env_vars";
 
 ENV_VARS.spannerInstanceId = "test";
-ENV_VARS.gcsVideoBucketName = "phading-dev-video";
-ENV_VARS.r2VideoBucketName = "video-dev";
+ENV_VARS.r2VideoBucketName = "video-test";
+ENV_VARS.gcsVideoBucketName = "phading-test-video";
 ENV_VARS.gcsVideoMountedLocalDir = "./gcs_video";
